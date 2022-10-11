bc6512082049653b80c17eef830fc6257aff9c37
orviz <orviz@ifca.unican.es>
2022-10-11 11:15:09 +0200

    Add JePL file structure

---
+++ .sqa/script.slaphappy-puce-maltese.sh
@@ -0,0 +1,4 @@
+(
+cd git.scc.kit.edu/synergy.o3as/o3skim &&
+    find_doc_files.py --file_type all
+)
---
+++ .sqa/script.wheezy-cornflower-argentino.sh
@@ -0,0 +1,4 @@
+(
+cd git.scc.kit.edu/synergy.o3as/o3skim &&
+    bandit -f json --quiet  --confidence-level high --severity-level high --recursive .
+)
